e77d8100633078248b57de4c057f03de6355e813
Abdullah Darcin <abdullahdarcin01@gmail.com>
2021-12-26 23:29:02 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQ.Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
